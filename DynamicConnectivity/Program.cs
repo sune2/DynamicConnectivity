bca609d25fbb42b5abf68806856d2e4d4d7ab47a
Kazunori Tamura <tamura_kazunori@cyberagent.co.jp>
2017-11-23 17:55:01 +0900

    first commit

---
+++ DynamicConnectivity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DynamicConnectivity
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
